c20a5a093a1ae28ed5d50ca89d8c90279c853bfa
sh4869 <nobuk4869@gmail.com>
2014-11-02 23:29:17 +0900

    ショートカットキーが使えなくなる問題を解決しました(多分)

--- mikutter_covconv.rb
+++ mikutter_covconv.rb
@@ -4,7 +4,11 @@ require 'uri'
 require 'rexml/document' 
 
 Plugin.create(:mikutter_covconv) do
-  UserConfig[:covconv] = false 
+  UserConfig[:covconv] ||= false 
+
+  settings("けんこふ") do
+	boolean('けんこふ語を使用する',:covconv)
+  end
 
   filter_gui_postbox_post do |gui_postbox|
 	if UserConfig[:covconv] == true 
@@ -13,13 +17,10 @@ Plugin.create(:mikutter_covconv) do
 									 {'ja_JP'=> text})
 	  xml = REXML::Document.new(response.body)
 	  tweet = xml.elements['covconv/covlang'].text
-	  Service.primary.post :message => tweet
-	  Plugin.call(:before_postbox_post, tweet)
+	  Service.primary.update(message: tweet)
 	  Plugin.create(:gtk).widgetof(gui_postbox).widget_post.buffer.text = ''
 	  Plugin.filter_cancel!
 	end
   end
-  settings("けんこふ") do
-	boolean('けんこふ語を使用する',:covconv)
-  end
+
 end
